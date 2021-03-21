bb1f43841c65d7b10cfef9330e34bb51bc4fb4f4
Hassaan Asif Khan <hak12b@gmail.com>
2021-03-21 18:03:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ function handleFormSubmit(e) {
   var password_script = $("password").val();
   var c_password_script = $("#confirm_password").val();
   if (name_script.lenght < 5) {
-    $("#name").after(<p>Enter name with more than 5 letters</p>);
+    $("#name").after("<p>Enter name with more than 5 letters</p>");
     e.preventDefault();
   }
   if (password_script.lenght < 8) {
-    $("#password").after(<p>Enter password more than 8 letter</p>);
+    $("#password").after("<p>Enter password more than 8 letter</p>");
     e.preventDefault();
   }
   if (password_script != c_password_script) {
-    $("#confirm_password").after(<p>Enter same password</p>);
+    $("#confirm_password").after("<p>Enter same password</p>");
     e.preventDefault();
   }
 }
